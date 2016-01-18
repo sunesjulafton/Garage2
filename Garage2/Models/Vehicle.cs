ee0cb49b429b2005150f3b1187d4617e3900ea35
Niklas Strand <nikstra@users.noreply.github.com>
2016-01-18 09:47:03 +0100

    Initial database and skaffolding

---
+++ Garage2/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Controllers
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public VehicleTypes Type { get; set; }
+        public string RegNum { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int WheelCount { get; set; }
+    }
+}
---
+++ Garage2/Models/VehicleTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Controllers
+{
+    public enum VehicleTypes
+    {
+        Aeroplane,
+        Boat,
+        Bus,
+        Car,
+        Motorcycle
+    }
+}
