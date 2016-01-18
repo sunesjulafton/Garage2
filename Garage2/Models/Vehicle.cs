b0ada99ea0c8e495aa99242c0eac491fad02dffa
Niklas Strand <nikstra@users.noreply.github.com>
2016-01-18 11:44:25 +0100

    Renamed create/delete and changed links to Swedish.

--- Garage2/Models/Vehicle.cs
+++ Garage2/Models/Vehicle.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Garage2.Controllers
 {
     public class Vehicle
     {
         public int Id { get; set; }
+
+        [Display(Name = "Fordonstyp")]
         public VehicleTypes Type { get; set; }
+
+        [Display(Name = "Registreringsnummer")]
         public string RegNum { get; set; }
+
+        [Display(Name = "Märke")]
         public string Make { get; set; }
+
+        [Display(Name = "Modell")]
         public string Model { get; set; }
+
+        [Display(Name = "Antal hjul")]
         public int WheelCount { get; set; }
     }
 }
