2ba7e4294a6a3ec646636ca387eab13d694ddb5a
sune <sunesjulafton@gmail.com>
2016-01-19 15:25:54 +0100

    Added Appsettings

---
+++ Garage2/Configuration/appSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Configuration
+{
+    public class appSettings
+    {
+        public static int PricePerMinute()
+        {
+            string _price = System.Web.Configuration.WebConfigurationManager.AppSettings["pricePerMinute"];
+            if (_price != null)
+            {
+                return Convert.ToInt32(_price);
+            }
+            //Default parking price per minute
+            return 1;
+        }
+    }
+}
+
